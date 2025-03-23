cb9b5d83c13957b3a67244aae49b199110bd795f
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2025-03-23 17:13:11 +0000

    get gateway/billing done

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "gateway/billing done"
+git commit -m "get gateway/billing done"
 git push --force
